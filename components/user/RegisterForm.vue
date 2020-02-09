a07f74381fab626798de33ce9a3d0bbac88c724e
itcast-hsian <407775611@qq.com>
2020-02-09 11:08:12 +0800

    注册的表单验证

--- components/user/RegisterForm.vue
+++ components/user/RegisterForm.vue
@@ -7,14 +7,16 @@
         ref="form" 
         :rules="rules" 
         class="form">
-            <el-form-item class="form-item">
+            <el-form-item class="form-item" prop="username">
                 <el-input 
+                v-model="form.username"
                 placeholder="用户名手机">
                 </el-input>
             </el-form-item>
 
-            <el-form-item class="form-item">
+            <el-form-item class="form-item" prop="captcha">
                 <el-input 
+                v-model="form.captcha"
                 placeholder="验证码" >
                     <template slot="append">
                         <el-button @click="handleSendCaptcha">
@@ -24,21 +26,24 @@
                 </el-input>
             </el-form-item>
 
-            <el-form-item class="form-item">
+            <el-form-item class="form-item" prop="nickname">
                 <el-input 
+                v-model="form.nickname"
                 placeholder="昵称">
                 </el-input>
             </el-form-item>
 
-            <el-form-item class="form-item">
+            <el-form-item class="form-item" prop="password">
                 <el-input 
+                v-model="form.password"
                 placeholder="密码" 
                 type="password"
                 ></el-input>
             </el-form-item>
 
-            <el-form-item class="form-item">
+            <el-form-item class="form-item" prop="checkPassword">
                 <el-input 
+                v-model="form.checkPassword"
                 placeholder="确认密码" 
                 type="password">
                 </el-input>
@@ -56,11 +61,62 @@
 <script>
 export default {
     data(){
+        // rule当前的规则（可忽略）
+        // value是输入框的值
+        // callback 比如要执行，如果传入一个错误的对象，当前的字段就会报错
+        const validatePass = (rule, value, callback) => {
+            if (value === '') {
+                callback(new Error('请输入密码'));
+            } else {
+                if (this.form.checkPassword !== '') {
+                    // 验证和确认密码是否一致, 手动触发确认密码验证
+                    this.$refs.form.validateField('checkPassword');
+                }
+                callback();
+            }
+        };
+
+        // 验证确认密码
+        const validateCheckPass = (rule, value, callback) => {
+            if (value === '') {
+                callback(new Error('请再次输入密码'));
+            } else if (value !== this.form.password) {
+                callback(new Error('两次输入密码不一致!'));
+            } else {
+                callback();
+            }
+        };
+
         return {
             // 表单数据
-            form: {},
+            form: {
+                username: "",
+                captcha: "",
+                nickname: "",
+                password: "",
+                checkPassword: ""
+            },
             // 表单规则
-            rules: {},
+            rules: {
+                username: [
+                    // required表示必填，message错误的提示信息. trigger输入框失去焦点时候触发验证
+                    { required: true, message: '请输入用户名', trigger: 'blur' }
+                ],
+                captcha: [
+                    { required: true, message: '请输入验证码', trigger: 'blur' }
+                ],
+                nickname: [
+                    { required: true, message: '请输入昵称', trigger: 'blur' }
+                ],
+
+                password: [
+                    // validator是验证的函数，输入框失去焦点时候触发函数执行
+                    { validator: validatePass, trigger: 'blur' }
+                ],
+                checkPassword: [
+                     { validator: validateCheckPass, trigger: 'blur' }
+                ],
+            },
         }
     },
     methods: {
@@ -69,10 +125,18 @@ export default {
 
         },
 
-
         // 注册
         handleRegSubmit(){
-           console.log(this.form)
+            this.$refs.form.validate(valid => {
+                // 表单验证全部通过
+                if(valid){
+                    // 调用注册的接口
+                    console.log(this.form)
+                }
+            })
+
+
+           
         }
     }
 }
