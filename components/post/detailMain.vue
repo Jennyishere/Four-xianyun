e750b418b2163356fa2cdd8048416f05cb138c16
Cuar <46847886+Cuar@users.noreply.github.com>
2020-02-24 00:09:00 +0800

    评论提交/评论获取

--- components/post/detailMain.vue
+++ components/post/detailMain.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!-- mian -->
-    <div>
+    <div :v-model="form" ref="form">
       攻略:
       <span v-html="detailList[0].city.created_at" v-if="detailList[0]"></span>
       阅读:
@@ -14,30 +14,60 @@
     <!-- footer -->
     <div>
       <!-- 输入框 -->
-      
-      <div class="ip" >
+
+      <div class="ip">
         <span class="right">评论</span>
-        <el-input type="textarea" :rows="2" placeholder="请输入内容" v-model="textarea"></el-input>
+        <el-input type="textarea" :rows="2" placeholder="请输入内容" v-model="form.content"></el-input>
       </div>
       <!-- 上传 按钮 -->
-  <div>
-
-    <el-upload
+      <div>
+        <el-upload
   action="https://jsonplaceholder.typicode.com/posts/"
   list-type="picture-card"
   :on-preview="handlePictureCardPreview"
-  :on-remove="handleRemove">
+  :on-remove="handleRemove"
+  v-model="form.pics"
+  >
   <i class="el-icon-plus"></i>
 </el-upload>
 <el-dialog :visible.sync="dialogVisible" size="tiny">
   <img width="100%" :src="dialogImageUrl" alt="">
 </el-dialog>
-<!-- 按钮 -->
-<el-button type="success">提交</el-button>
-  </div>
+        <!-- 按钮 -->
+        <el-button type="success" @click='submit'>提交</el-button>
+      </div>
       <!-- 评论区 -->
-      
     </div>
+    <div>
+   <div class="keeps">
+      <h2>评论</h2>
+      <div class="item">
+        <div class="head">
+          <img src="../../images/01.jpg" alt />
+          <div>
+            <p>火星网友</p>
+            <span>2小时前</span>
+          </div>
+          <span>回复</span>
+        </div>
+        <div class="text">文章说得很有道理</div>
+      </div>
+      <div class="more">更多跟帖</div>
+    </div>
+    <!-- 分页 -->
+    <div class="block">
+    
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="currentPage4"
+      :page-sizes="[100, 200, 300, 400]"
+      :page-size="100"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="400">
+    </el-pagination>
+  </div>
+ </div>
   </div>
 </template>
 
@@ -46,16 +76,21 @@ export default {
   data() {
     return {
       detailList: [],
-         imageUrl: '',
-         textarea: '',
           dialogImageUrl: '',
-        dialogVisible: false
+        dialogVisible: false,
+
+      form: {
+        content:'',
+        pics:[],
+        id:''
+      },
+      
     };
   },
   mounted() {
     const { id } = this.$route.query;
     //console.log(id)
-
+    this.form.id =id
     //请求文章详情
     this.$axios({
       url: "/posts",
@@ -69,31 +104,61 @@ export default {
       this.detailList = data;
       // console.log(this.detailList)
     });
+    //
+      //获取评论
+      this.$axios({
+        url: "/posts/comments",
+        params: {
+          post :id,
+         _start: 0,
+         _limit: 5,
+         
+
+        }
+      }).then(res => {
+        console.log(res)
+
+        // console.log(this.detailList)
+      });
   },
-     methods: {
-      handleRemove(file, fileList) {
+  methods: {
+    //分页
+        handleSizeChange(val) {
+      console.log(`每页 ${val} 条`);
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`);
+    },
+  handleRemove(file, fileList) {
         console.log(file, fileList);
       },
       handlePictureCardPreview(file) {
         this.dialogImageUrl = file.url;
         this.dialogVisible = true;
       },
-      Tips(){
-           this.$message({
-          message: '暂时不支持分享',
-          type: 'warning'
-        });
-      }
+    Tips() {
+      this.$message({
+        message: "暂时不支持分享",
+        type: "warning"
+      });
+    },
+    submit(){
+      this.$axios({
+        url:'/comments',
+        methods:'POST',
+        data:this.form 
+      }).then(res =>{
+        console.log(res)
+      })
     }
-  
+  }
 };
 </script>
 
 <style scoped lang="less">
-i{
-      cursor: pointer;
+i {
+  cursor: pointer;
 }
-
 
 .el-row {
   margin-bottom: 20px;
@@ -122,27 +187,81 @@ i{
   background-color: #f9fafc;
 }
 // 上传
- .avatar-uploader .el-upload {
-    border: 1px dashed #d9d9d9;
-    border-radius: 6px;
-    cursor: pointer;
-    position: relative;
-    overflow: hidden;
-  }
-  .avatar-uploader .el-upload:hover {
-    border-color: #409EFF;
-  }
-  .avatar-uploader-icon {
-    font-size: 28px;
-    color: #8c939d;
-    width: 178px;
-    height: 178px;
-    line-height: 178px;
+.avatar-uploader .el-upload {
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+.avatar-uploader .el-upload:hover {
+  border-color: #409eff;
+}
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  line-height: 178px;
+  text-align: center;
+}
+.avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
+}
+//评论区
+.keeps {
+  border-top: 5px solid #ddd;
+  padding: 0 15px 50px;
+  > h2 {
+    line-height: 50px;
     text-align: center;
   }
-  .avatar {
-    width: 178px;
-    height: 178px;
-    display: block;
+  .item {
+    padding: 10px 0;
+    border-bottom: 1px solid #ccc;
+    .head {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      > img {
+        width: 50/360 * 100vw;
+        height: 50/360 * 100vw;
+        display: block;
+        border-radius: 50%;
+      }
+      > div {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        margin-left: 10px;
+        > span {
+          font-size: 12px;
+          color: #999;
+          line-height: 25px;
+        }
+      }
+      > span {
+        color: #999;
+        font-size: 13px;
+      }
+    }
+    .text {
+      font-size: 14px;
+      color: #333;
+      padding: 20px 0 10px 0;
+    }
   }
+  .more {
+    width: 100px;
+    height: 30px;
+    line-height: 30px;
+    text-align: center;
+    border: 1px solid #ccc;
+    border-radius: 15px;
+    margin: 20px auto;
+    font-size: 13px;
+  }
+}
 </style>
