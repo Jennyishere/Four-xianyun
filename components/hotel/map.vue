8c5ef711733ef5f2e225c61b9a2e72dc2775d25b
Jeny <853297695@qq.com>
2020-02-24 00:44:09 +0800

    地图关联

--- components/hotel/map.vue
+++ components/hotel/map.vue
@@ -89,15 +89,13 @@ export default {
           // 定位出错
         }
       });
+      var url =
+        "https://webapi.amap.com/maps?v=1.4.15&key=39406c0b73d92c64b2365096ff6d0236&callback=onLoad";
+      var jsapi = document.createElement("script");
+      jsapi.charset = "utf-8";
+      jsapi.src = url;
+      document.head.appendChild(jsapi);
     }
-    console.log(this.currentCity); //空
-
-    var url =
-      "https://webapi.amap.com/maps?v=1.4.15&key=39406c0b73d92c64b2365096ff6d0236&callback=onLoad";
-    var jsapi = document.createElement("script");
-    jsapi.charset = "utf-8";
-    jsapi.src = url;
-    document.head.appendChild(jsapi);
   },
   methods: {
     open(data) {
