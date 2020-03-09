a661de73ded166e623c38808e962b9228700c25d
Cuar <46847886+Cuar@users.noreply.github.com>
2020-03-10 02:01:42 +0800

    test

--- pages/post/detail.vue
+++ pages/post/detail.vue
@@ -1,7 +1,7 @@
 <template>
   <div post-detail>
     <el-col :span="17" class="detail-content"><div class="detail-content">正文</div></el-col>
-  <el-col :span="7"><div >相关推荐</div></el-col>
+  <el-col :span="7"><div >相关推荐</div></el-col>1
   </div>
 </template>
 
